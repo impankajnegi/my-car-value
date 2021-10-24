1fec90663e1f02103caa5ceb0af1377e468ec849
Pankaj Negi <pnegi.dun@gmail.com>
2021-10-24 16:11:43 +0530

    Added password hashing with salt for sign up user

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,6 +1,10 @@
 import { BadRequestException, Injectable } from "@nestjs/common"; 
 import { UsersService } from "./users.service";
+import { randomBytes, scrypt as _scrypt } from "crypto";
+import { promisify} from 'util'
+import { runInThisContext } from "vm";
 
+const scrypt = promisify(_scrypt)
 
 @Injectable()
 export class AuthService{
@@ -13,10 +17,16 @@ export class AuthService{
             throw new BadRequestException('Email in use')
         }
         //Hash user's password
-        
+        //1. Generate a salt
+        const salt = randomBytes(8).toString('hex');
+        //2. Hash the salt and password together
+        const hash =( await scrypt(password, salt, 32)) as Buffer
+        //3. Join the hash result and salt together
+        const result = salt+"."+hash.toString('hex');
         //Create a new user and sane it
-
+        const user = await this.userService.create(email, result)
         //Return the user
+        return user;
     }
     
     signin(){
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body, Get, Patch, Query, Param, Delete, 
 UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common'; 
 import { SerializeInterceptor, Serialize } from 'src/interceptors/serialize.interceptor';
+import { AuthService } from './auth.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user-dto';
 import { UserDto } from './dtos/user.dto';
@@ -10,11 +11,13 @@ import { UsersService } from './users.service';
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-   constructor(private userService:UsersService){}
+   constructor(private userService:UsersService, private authService:AuthService){}
 
  @Post('/signup')
  createUser(@Body() body: CreateUserDto){
-    this.userService.create(body.email, body.password)
+   //  this.userService.create(body.email, body.password)
+   return this.authService.signup(body.email, body.password)
+
  }
 
 //@UseInterceptors(new SerializeInterceptor(UserDto))
